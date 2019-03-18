6c7ec6ed5337200f16c46a179da1e205860b1a28
valdomirocm2016 <valdomirocm@gmail.com>
2019-03-18 20:05:47 -0300

    Commit da Versão  Template Method

---
+++ src/templateMethod/Principal.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package templateMethod;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author Valdomiro
+ */
+public class Principal {
+     public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        Map<String,Object> mapa= new HashMap();
+        mapa.put("Nome", "Valdomiro");
+        mapa.put("Idade", "x");
+        GeradorArquivo gerador= new GeradorPropriedadesCriptografado();
+        gerador.gerarArquivo("..\\TemplateMethod\\src\\templateMethod\\arquivos\\Criptografado.txt", mapa);
+        
+        GeradorArquivo gerador2 = new GeradorXmlCompactado();
+        gerador.gerarArquivo("..\\TemplateMethod\\src\\templateMethod\\arquivos\\Xml.zip", mapa);
+        
+        /*GeradorArquivo ger= new GeradorArquivo();
+        ger.gerarArquivo("c:\\Gerador\\Xml.zip", mapa);*/
+        
+    }
+}
